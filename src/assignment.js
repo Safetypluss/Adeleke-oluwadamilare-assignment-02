9e8a1b8c51c236c25a7c931a0345aebab301fb44
Oluwadamilare Adeleke <108585711+Safetypluss@users.noreply.github.com>
2022-07-10 11:09:43 +0000

    check challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -4,15 +4,13 @@ const assignment = {};
 // ========================
 
 
-/**
- * Challenge - 1
- * 
- * Calculate and return the sum of the numbers in an array.
- * If you did Challenge - 1, remove the comment in the line just after this function
- * 
- * @param {Array} arrayOfNumbers the array of numbers to sum
- * @returns number the sum of the numbers
- */
+const array= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50];
+let sum = 0;
+
+for (let i = 0; i < array.length; i++) {
+  sum +=array[i];
+} 
+console.log(sum);
 function sumOfNumbers(arrayOfNumbers) {
     return arrayOfNumbers.length;
 }
@@ -22,11 +20,21 @@ function sumOfNumbers(arrayOfNumbers) {
  * Challenge - 2
  * 
  * Count all even numbers within an array of numbers.
- * If you did Challenge - 2, remove the comment in the line just after this function
- * 
- * @param {Array} arrayOfNumbers the array containing even or non-even numbers
- * @returns number the count of even numbers
- */
+ * If you did Challenge - 2, remove the comment in the line just after this function*/
+  var tall = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50];
+
+liste(tall);
+
+function liste(arr) {
+  var sumeven = 0;
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumeven++;
+    } 
+  }
+  console.log("even : " + sumeven);
+}
 function countEvenNumbers(arrayOfNumbers) {
     return arrayOfNumbers.length;
 }
