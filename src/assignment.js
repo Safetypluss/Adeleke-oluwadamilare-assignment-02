0854993934d3e2293614993c198170854a2e2348
Oluwadamilare Adeleke <108585711+Safetypluss@users.noreply.github.com>
2022-07-11 09:52:35 +0000

    review challenge 1, 2

--- src/assignment.js
+++ src/assignment.js
@@ -2,11 +2,8 @@
 // DO NOT EDIT THIS BLOCK
 const assignment = {};
 // ========================
-
-
 const array= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50];
 let sum = 0;
-
 for (let i = 0; i < array.length; i++) {
   sum +=array[i];
 } 
@@ -15,19 +12,15 @@ function sumOfNumbers(arrayOfNumbers) {
     return arrayOfNumbers.length;
 }
 // assignment.sumOfNumbers = sumOfNumbers;
-
 /**
  * Challenge - 2
  * 
  * Count all even numbers within an array of numbers.
  * If you did Challenge - 2, remove the comment in the line just after this function*/
-  var tall = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50];
-
+var tall = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50];
 liste(tall);
-
 function liste(arr) {
   var sumeven = 0;
-
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] % 2 === 0) {
       sumeven++;
@@ -38,8 +31,6 @@ function liste(arr) {
 function countEvenNumbers(arrayOfNumbers) {
     return arrayOfNumbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
-
 /**
  * Challenge - 3
  * 
